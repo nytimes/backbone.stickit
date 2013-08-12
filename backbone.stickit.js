759dcd9dff5112266e6cf58615df1c197eaf8812
Andrey Goryachev <andrey@sixdays.ru>
2013-08-12 17:52:05 +0400

    if observe in bindigns defines as array of attributes, onSet can return array of values for those attributes

--- backbone.stickit.js
+++ backbone.stickit.js
@@ -180,7 +180,20 @@
 
   // Prepares the given `val`ue and sets it into the `model`.
   var setAttr = function(model, attr, val, options, context, config) {
-    if (config.onSet) val = applyViewFn(context, config.onSet, val, config);
+    if (config.onSet) {
+        val = applyViewFn(context, config.onSet, val, config);
+        // if observe var defined as array, prepare attrs instead of attr and val
+        if (_.isArray(attr)) {
+            var attrs = {};
+            _.each(attr, function(attribute, index) {
+                attrs[attribute] = _.has(val, index) ? val[index] : val;
+            });
+
+            model.set(attrs, options);
+            return;
+        }
+    }
+
     model.set(attr, val, options);
   };
 
