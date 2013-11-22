db8db06fe3c899f8f5b63e5ea3d859d77812ffa3
Adam Krebs <amk528@cs.nyu.edu>
2013-11-22 03:05:26 -0500

    fix qunit complaining about infinite stack. compare cids for view equality

--- test/bindData.js
+++ test/bindData.js
@@ -1390,7 +1390,7 @@ test('bindings:selectOptions:defaultOption:OptGroups', 8, function() {
       '#test1': {
         observe: 'water',
         updateModel: function(val, options) {
-          equal(this, view);
+          equal(this.cid, view.cid);
           equal(val, view.$('#test1').val());
           equal(options.observe, 'water');
           return val == 'evian';
@@ -1485,7 +1485,7 @@ test('bindings:selectOptions:defaultOption:OptGroups', 8, function() {
       '#test1': {
         observe: 'water',
         getVal: function($el, event, options) {
-          equal(this, view);
+          equal(this.cid, view.cid);
           equal($el.attr('id'), 'test1');
           equal(event.type, 'change');
           equal(options.observe, 'water');
